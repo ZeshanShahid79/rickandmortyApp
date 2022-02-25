6d7b6713af63ea85bc091ee21f28c0f2efdb87d2
Zaid Lonne <zaid.lonne@gmail.com>
2022-02-25 13:04:01 +0100

    add character.episode

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+console.clear()
 const apiUrl = 'https://rickandmortyapi.com/api/character';
 
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => createCharacterList(data.results));
+.then(response => response.json())
+.then(data => createCharacterList(data.results));
 
 function createCharacterList(characters) {
   const listElement = document.createElement('ul');
   listElement.className = 'card-list';
   document.body.append(listElement);
+  
+/*
+function createEpisodeList (episodes) {
+  episodes.forEach((episode) => {
+    const episodeUrl = episode.episode[0];
+    console.log(episodeUrl)
+    fetch(episodeUrl)
+    .then((response) => response.json())
+    .then((pokeData) => createCharacterList(pokeData));
+  });
+}
+*/
 
   characters.forEach(character => {
     const card = document.createElement('li');
@@ -16,8 +29,12 @@ function createCharacterList(characters) {
     <h2 class="card__name">${character.name}</h2>
     <img class="card__image" src=${character.image} alt="Picture of ${character.name}">
     <span class="card__statspec">${character.species} - ${character.status}</span> 
-    <span class="card__origin"
+    <span class="card__origin">${character.origin.name}</span>
     `;
+
+fetch(character.episode[0])
+  .then(response => response.json())
+  .then(data => console.log(data))
 
     listElement.append(card);
   });
